bf9b75b5dcd07cc32d09fbd7bd108a5f3076d9ef
Sungchanyoung <chanyouunng1@gmail.com>
2025-11-09 22:43:29 +0900

    fixed3

--- pyback/generate_report.py
+++ pyback/generate_report.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, Dashboard!"
+    # return "Hello, Dashboard!" (이전 코드)
+    return render_template('index.html') # 수정된 코드
 
 if __name__ == '__main__':
     app.run(debug=True)
